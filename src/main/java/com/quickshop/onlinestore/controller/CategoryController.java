8629ce08bc73474b5508ce5814f5b44348b90702
thara-1234 <tharakrishna1989@gmail.com>
2024-12-29 18:02:12 +0530

    Category Added

--- src/main/java/com/quickshop/onlinestore/controller/CategoryController.java
+++ src/main/java/com/quickshop/onlinestore/controller/CategoryController.java
@@ -1,2 +1,24 @@
-package com.quickshop.onlinestore.controller;public class CategoryController {
+package com.quickshop.onlinestore.controller;
+
+import com.quickshop.onlinestore.model.Category;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CategoryController {
+    private List<Category> categories=new ArrayList<>();
+    @GetMapping("/api/public/categories")
+    public List<Category> getAllCategories(){
+        return categories;
+    }
+    @PostMapping("/api/public/categories")
+    public String addCategory(@RequestBody Category category){
+        categories.add(category);
+        return "Category added successfully";
+    }
 }
