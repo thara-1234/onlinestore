f9aa9111864215d252094705dd2c7026dd65179f
thara-1234 <tharakrishna1989@gmail.com>
2024-12-30 13:16:51 +0530

    Update  and Request Mapping annotation added

--- src/main/java/com/quickshop/onlinestore/controller/CategoryController.java
+++ src/main/java/com/quickshop/onlinestore/controller/CategoryController.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/public")
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
@@ -19,26 +20,36 @@ public class CategoryController {
 //    public CategoryController(CategoryService categoryService) {
 //        this.categoryService = categoryService;
 //    }
-
-    @GetMapping("/api/public/categories")
+//@RequestMapping(value = "/api/public/categories",method = RequestMethod.GET)
+    @GetMapping("/categories")
     public ResponseEntity<List<Category>> getAllCategories(){
 
         List<Category> categories = categoryService.getAllCategories();
         return new ResponseEntity<>(categories,HttpStatus.OK);
     }
-    @PostMapping("/api/public/categories")
+    @PostMapping("/categories")
     public ResponseEntity<String> addCategory(@RequestBody Category category){
         categoryService.createCategory(category);
         //return "Category added successfully";
         return new ResponseEntity<>("Category added successfully",HttpStatus.CREATED);
     }
-    @DeleteMapping("/api/public/categories/{categoryId}")
+    @DeleteMapping("/categories/{categoryId}")
     public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId){
         try {
             String status = categoryService.deleteCategory(categoryId);
             return new ResponseEntity<>(status, HttpStatus.OK);
             //return ResponseEntity.ok(status);
             //return ResponseEntity.status(HttpStatus.OK).body(status);
+        }
+        catch (ResponseStatusException e){
+            return new ResponseEntity<>(e.getReason(),e.getStatusCode());
+        }
+    }
+    @PutMapping("/categories/{categoryId}")
+    public ResponseEntity<String> updateCategory(@RequestBody Category category,@PathVariable Long categoryId){
+        try {
+            Category savedCategory = categoryService.updateCategory(category,categoryId);
+            return new ResponseEntity<>("Category with categoryId "+categoryId, HttpStatus.OK);
         }
         catch (ResponseStatusException e){
             return new ResponseEntity<>(e.getReason(),e.getStatusCode());
--- src/main/java/com/quickshop/onlinestore/service/CategoryServiceImpl.java
+++ src/main/java/com/quickshop/onlinestore/service/CategoryServiceImpl.java
@@ -7,6 +7,8 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class CategoryServiceImpl implements CategoryService{
     private List<Category> categories=new ArrayList<>();
@@ -32,6 +34,22 @@ public class CategoryServiceImpl implements CategoryService{
                 .findFirst().orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND,"Resource not found"));
         categories.remove(category);
         return "Category with categoryId "+categoryId+" deleted successfully";
+    }
+
+    @Override
+    public Category updateCategory(Category category, Long categoryId) {
+        Optional<Category> optionalCategory=categories.stream()
+                .filter(c->c.getCategoryId().equals(categoryId))
+                .findFirst();
+        if(optionalCategory.isPresent()){
+            Category existingCategory=optionalCategory.get();
+            existingCategory.setCategoryName(category.getCategoryName());
+            return existingCategory;
+        }
+        else {
+          throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Category not found");
+        }
+
     }
 
 
