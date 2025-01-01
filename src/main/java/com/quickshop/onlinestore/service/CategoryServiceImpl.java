a5c4239130f895d8d9389379f64d2f9ac6c407e6
thara-1234 <tharakrishna1989@gmail.com>
2025-01-01 15:14:51 +0530

    Repositories created and data storing in database

--- src/main/java/com/quickshop/onlinestore/service/CategoryServiceImpl.java
+++ src/main/java/com/quickshop/onlinestore/service/CategoryServiceImpl.java
@@ -6,26 +6,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class CategoryServiceImpl implements CategoryService{
-    private List<Category> categories=new ArrayList<>();
-    private Long nextId= 1L;
+    //private List<Category> categories=new ArrayList<>();
+   // private Long nextId= 1L;
     @Autowired
     private CategoryRepository categoryRepository;
     @Override
     public List<Category> getAllCategories() {
-        return categories;
+        //return categories;
+        return categoryRepository.findAll();
     }
 
     @Override
     public void createCategory(Category category) {
-        category.setCategoryId(nextId++);
-        categories.add(category);
+        //category.setCategoryId(nextId++);
+        //categories.add(category);
+        categoryRepository.save(category);
 
     }
 
@@ -33,6 +33,7 @@ public class CategoryServiceImpl implements CategoryService{
     public String deleteCategory(Long categoryId) {
         //Category category=categories.stream().filter(c->c.getCategoryId().equals(categoryId)).findFirst().get();
         //Category category=categories.stream().filter(c->c.getCategoryId().equals(categoryId)).findFirst().orElse(null);
+       List<Category> categories=categoryRepository.findAll();
         Category category=categories.stream()
                 .filter(c->c.getCategoryId().equals(categoryId))
                 .findFirst().orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND,"Resource not found"));
@@ -42,13 +43,14 @@ public class CategoryServiceImpl implements CategoryService{
 
     @Override
     public Category updateCategory(Category category, Long categoryId) {
+        List<Category> categories=categoryRepository.findAll();
         Optional<Category> optionalCategory=categories.stream()
                 .filter(c->c.getCategoryId().equals(categoryId))
                 .findFirst();
         if(optionalCategory.isPresent()){
             Category existingCategory=optionalCategory.get();
             existingCategory.setCategoryName(category.getCategoryName());
-            return existingCategory;
+            return categoryRepository.save(existingCategory);
         }
         else {
           throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Category not found");
